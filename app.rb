7c2083610c2247c4dfdd32db0b51095af792bf76
David Zheng <davidzheng@knights.ucf.edu>
2017-07-05 18:07:59 -0400

    post order and delete button fix

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ before do
 end
 
 get '/' do
-  @posts = Post.all
+  @posts = Post.order( id: :desc )
   erb :home
 end
 
@@ -84,7 +84,7 @@ end
 get '/post/:id/delete' do
   @post = Post.find(params[:id])
   if @post.user_id != @current_user.id
-    flash[:message] = "This is not your post!"
+    redirect '/'
   elsif @post.destroy
     flash[:message] = "Message deleted."
     redirect '/'
